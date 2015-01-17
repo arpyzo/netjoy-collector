081abed6ff692b97efac900f31d85958cb153616
Robert Pyzalski <pyzals@gmail.com>
2015-01-17 17:21:23 -0500

    testing signal handling

--- netjoy.c
+++ netjoy.c
@@ -3,9 +3,14 @@
 
 char error_buffer[PCAP_ERRBUF_SIZE];
 
+void packet_handler(unsigned char *param, const struct pcap_pkthdr *header, const unsigned char *pkt_data) {
+    printf("Processed packet\n");
+}
+
 int main() {
     pcap_if_t *nic_list;
     pcap_if_t *nic;
+    pcap_t *nic_handle;
 
     if (pcap_findalldevs(&nic_list, error_buffer) == -1) {
         printf("Failed to retrieve network card list!\n");
@@ -22,4 +27,17 @@ int main() {
     }
 
     pcap_freealldevs(nic_list);
+    
+    if ((nic_handle = pcap_create("eth0", error_buffer)) == NULL) {
+        printf("Unable to open network adapter!");
+        return 1;
+    }
+    pcap_set_promisc(nic_handle, 1);
+    // TODO: set minimum size for capture
+    pcap_set_snaplen(nic_handle, 1000);
+    pcap_activate(nic_handle);
+    
+    pcap_loop(nic_handle, -1, packet_handler, NULL);
+
+    return 0;
 }
